73e24283ac8f775ab067e3f532a1cee25d894c7d
Marek Lewandowski <marek@mlewandowski.com>
2018-11-16 00:29:34 +0100

    Adjusted types.

--- Elastic/Out.h
+++ Elastic/Out.h
@@ -8,9 +8,9 @@ template < typename NumericType >
 class TransformationElasticOut : public TransformationBase< NumericType > {
 public:
 	virtual NumericType ease( NumericType t, NumericType b, NumericType c, NumericType d ) {
-		float s = 1.70158;
-		float p = 0;
-		float a = c;
+		NumericType s = 1.70158;
+		NumericType p = 0;
+		NumericType a = c;
 		if (t == 0)
 			return b;
 		if ((t /= d) == 1)
@@ -20,10 +20,10 @@ public:
 		if (a < std::abs(c))
 		{
 			a = c;
-			float s = p / 4;
+			NumericType s = p / 4;
 		}
 		else
-			float s = p / (2 * M_PI) * asin(c / a);
+			NumericType s = p / (2 * M_PI) * asin(c / a);
 		return a * pow(2, -10 * t) * sin((t * d - s) * (2 * M_PI) / p) + c + b;
 	};
 };
