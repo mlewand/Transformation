e46f9cec359949b4d0f04cc697971dabb2084c66
Marek Lewandowski <marek@mlewandowski.com>
2019-08-04 22:33:00 +0200

    Fixed compiler warning in Elastic/In.h as well.

--- Elastic/In.h
+++ Elastic/In.h
@@ -24,7 +24,10 @@ public:
 		}
 		else
 			s = p / (2 * M_PI) * asin(c / a);
-		return -(a * pow(2, 10 * (t -= 1)) * sin((t * d - s) * (2 * M_PI) / p)) + b;
+
+		t = t - 1;
+
+		return -(a * pow(2, 10 * t) * sin((t * d - s) * (2 * M_PI) / p)) + b;
 	};
 };
 
